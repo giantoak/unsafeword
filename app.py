b4ad8157a216eb8faaf0f030f3d5972024e22db9
Pete[r] M. Landwehr <pmlandwehr@gmail.com>
2015-09-29 19:48:51 -0400

    Basic app and requirements list

--- app.py
+++ app.py
@@ -1,18 +1,31 @@
+from elasticsearch import Elasticsearch
 from flask import Flask
 from flask import jsonify
 import pickle
 app = Flask(__name__)
 
-
 bad_dict = pickle.load(open('all_urls.pkl', 'rb'))
+# this is a URI for ISI (I *believe*, nothing else.
 
+@app.route('/isi/<path:isi_uri>')
+def check_isi_ad():
 
-@app.route('/')
-def check_ad_text():
-    ad_text = 'just some test text. XXXX!'
+    es = Elasticsearch(['https://darpamemex:darpamemex@esc.memexproxy.com/dig-latest/WebPage/_search'],
+                       verify_certs=False)
 
-    # Keep tokens with at least one . in them
-    toks = set(x for x in ad_text.strip().split() if x.find('.') > -1)
+    res = es.search(q='id:"{}"'.format(isi_uri))
+
+    text = ''
+    for key in ['hasTitlePart', 'hasBodyPart']:
+        try:
+            text += res['hits']['hits'][0]['_source'][key].lower().strip()
+            text += ' '
+        except AttributeError:
+            # skip if no title or body
+            pass
+
+    # Tokenize the title and text, keeping tokens with at least one . in them
+    toks = set(x for x in text.strip().split() if x.find('.') > -1)
 
     match_dict = {tok: bad_dict[tok] for tok in toks if tok in bad_dict}
     return jsonify(match_dict)
