fae16aba40c2e62c453a1dd64f5cebfa66982e9a
Pete[r] M. Landwehr <pmlandwehr@gmail.com>
2015-09-30 16:42:05 -0400

    Uploaded a working prototype; pickle not included.

--- app.py
+++ app.py
@@ -4,23 +4,32 @@ from flask import jsonify
 import pickle
 app = Flask(__name__)
 
-bad_dict = pickle.load(open('all_urls.pkl', 'rb'))
-# this is a URI for ISI (I *believe*, nothing else.
+bad_dict = pickle.load(open('all_urls_2.pkl', 'rb'))
+
+isi_es = Elasticsearch(['https://darpamemex:darpamemex@esc.memexproxy.com/dig-latest/WebPage/'], verify_certs=False)
+
+
+@app.route("/")
+def confirm():
+    return "The server's up."
+
+@app.route('/isi')
+def explain_isi():
+    return "This app takes a uri field for ISI's Elasticsearch endpoint: isi/[uri]"
 
 @app.route('/isi/<path:isi_uri>')
-def check_isi_ad():
+def check_isi_ad(isi_uri):
 
-    es = Elasticsearch(['https://darpamemex:darpamemex@esc.memexproxy.com/dig-latest/WebPage/_search'],
-                       verify_certs=False)
+    text_fields = ['hasTitlePart.text', 'hasBodyPart.text']
 
-    res = es.search(q='id:"{}"'.format(isi_uri))
+    res = isi_es.search(q='uri:"{}"'.format(isi_uri), fields=text_fields)
 
-    text = ''
-    for key in ['hasTitlePart', 'hasBodyPart']:
+    text = u''
+    for key in text_fields:
         try:
-            text += res['hits']['hits'][0]['_source'][key].lower().strip()
+            text += res['hits']['hits'][0]['fields'][key].lower().strip()
             text += ' '
-        except AttributeError:
+        except (IndexError, AttributeError):
             # skip if no title or body
             pass
 
@@ -28,6 +37,7 @@ def check_isi_ad():
     toks = set(x for x in text.strip().split() if x.find('.') > -1)
 
     match_dict = {tok: bad_dict[tok] for tok in toks if tok in bad_dict}
+    match_dict['uri'] = isi_uri
     return jsonify(match_dict)
 
 if __name__ == '__main__':
